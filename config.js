d5933fa545c4370b0652e6ebedbba27e1d515b50
johnnyboy1815-ctrl <johnnyboy1815@gmail.com>
2026-09-21 17:27:08 -0500

    Add files via upload
    Launch Mission Control v2

---
+++ config.js
@@ -0,0 +1,7 @@
+// Mission Control cloud configuration.
+// Leave blank for the first GitHub Pages deployment.
+// We will connect this to Supabase in the next setup step.
+window.BIOCUBE_CONFIG = {
+  supabaseUrl: "",
+  supabaseAnonKey: ""
+};
